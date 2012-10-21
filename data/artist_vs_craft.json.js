6a1e10756a768d672e2576434a001109c772f151
Chris Spurgeon <chris@spurgeonworld.com>
2012-10-20 19:02:11 -0700

    Added the brain example and the brain images.

---
+++ data/artist_vs_craft.json.js
@@ -0,0 +1,20 @@
+{
+    "crafts":
+    {
+        "description": "Craft artists",
+        "years":
+        [
+        ]
+    },
+    "artists":
+    {
+        "description": "Fine artists, including painters, sculptors, and illustrators",
+        "years":
+        {
+            "2006":
+            {
+                "total_employment": "850"
+            }
+        }
+    }
+}
